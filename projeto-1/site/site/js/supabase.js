444b588bdb1e3f43a9f2464666c01cbd47935318
gutin007 <oaugustoantonio2010@gmail.com>
2026-06-05 02:16:37 -0300

    Delete projeto-1 directory

--- projeto-1/site/site/js/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
-
-export const supabase = createClient(
-  'https://kywgmckuoueojcsomaxu.supabase.co',
-  'sb_publishable_yiYB2GLHEjwcSlhvEmAig_EHYqHKyE'
-)
